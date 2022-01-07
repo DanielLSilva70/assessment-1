d45305eaa1200cc8b29354b3d3bb886fa850145c
Daniel Silva <silvaldaniel70@gmail.com>
2022-01-07 12:08:32 -0600

    Done

--- passwords.js
+++ passwords.js
@@ -1,7 +1,19 @@
 let prompt = "hello enter your password"
 console.log(prompt)
 
-
+let length = "Password can have no more than 10 characters"
+console.log(length)
  
+
+let passwordEntry = "0123456789"
+console.log(passwordEntry)
+
+
+if (passwordEntry.length <= 10)
+    console.log("valid")
+
+if (passwordEntry.length > 10) 
+    console.log('invaild')
+    
 
 
