49d6b6b9451f267e7a9cc8f22fa020fd639a9bb6
Daniel Silva <silvaldaniel70@gmail.com>
2022-01-07 10:32:20 -0600

    sldc done

--- sldc.js
+++ sldc.js
@@ -19,3 +19,14 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+    console.log(planning);
+const analysisOfRequirment = "Detects what will be required to complete the code"
+    console.log(analysisOfRequirment);
+const design = 'the beginning of building the code'
+    console.log(design);
+const implementation = 'The process of checking the code while it is being made'
+    console.log(implementation);
+const testingIntegration = "making sure the code is working and fixing any bugs along the way"
+    console.log(testingIntegration);
+const maintain = "constantly checking if the code continues to work"
+    console.log(maintain);
